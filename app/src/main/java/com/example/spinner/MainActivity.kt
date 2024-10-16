c4fe9b0e78f79470883d6d6c3411fca80dda8150
haruka <b11107006@gapps.knu.edu.tw>
2024-10-16 12:30:36 +0800

    婚姻建議

--- app/src/main/java/com/example/spinner/MainActivity.kt
+++ app/src/main/java/com/example/spinner/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.spinner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spGender=findViewById<Spinner>(R.id.spinner)
+        val etAge=findViewById<EditText>(R.id.etAge)
+        val btOk=findViewById<Button>(R.id.btOk)
+        val tvR=findViewById<TextView>(R.id.tvR)
+
+        btOk.setOnClickListener {
+            val age=etAge.text.toString().toInt()
+            if (spGender.selectedItemPosition==0){
+                when{
+                    age<28 ->tvR.text=getString(R.string.no_hurry)
+                    age<=33 ->tvR.text=getString(R.string.find_couple)
+                    else ->tvR.text=getString(R.string.get_married)
+                }
+            }
+            else{
+                when{
+                    age<25 ->tvR.text=getString(R.string.no_hurry)
+                    age<=30 ->tvR.text=getString(R.string.find_couple)
+                    else ->tvR.text=getString(R.string.get_married)
+                }
+            }
+        }
     }
 }
